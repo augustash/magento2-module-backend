d0860f4cc655b3c6354e02458edfb8e633de3deb
Pete McWilliams <petemcw@gmail.com>
2020-02-07 10:10:57 -0600

    Adds ability to reindex from the admin Index Management screen

---
+++ Controller/Adminhtml/Indexer/MassReindex.php
@@ -0,0 +1,92 @@
+<?php
+
+/**
+ * August Ash Backend Module
+ *
+ * @author    Peter McWilliams <pmcwilliams@augustash.com>
+ * @copyright Copyright (c) 2020 August Ash (https://www.augustash.com)
+ */
+
+namespace Augustash\Backend\Controller\Adminhtml\Indexer;
+
+use Magento\Backend\App\Action;
+use Magento\Backend\App\Action\Context;
+use Magento\Indexer\Model\IndexerFactory;
+
+/**
+ * Re-index controller mass-action.
+ */
+class MassReindex extends Action
+{
+    /**
+     * @var \Magento\Indexer\Model\IndexerFactory
+     */
+    protected $indexerFactory;
+
+    /**
+     * Constructor.
+     *
+     * Initialize class dependencies.
+     *
+     * @param \Magento\Backend\App\Action\Context $context
+     * @param \Magento\Indexer\Model\IndexerFactory $indexerFactory
+     */
+    public function __construct(
+        Context $context,
+        IndexerFactory $indexerFactory
+    ) {
+        $this->indexerFactory = $indexerFactory;
+        parent::__construct($context);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function execute()
+    {
+        /** @var \Magento\Backend\Model\View\Result\Redirect $resultRedirect */
+        $resultRedirect = $this->resultRedirectFactory->create();
+        $resultRedirect->setPath('*/*/list');
+
+        $indexerIds = $this->getRequest()->getParam('indexer_ids');
+
+        if (!is_array($indexerIds)) {
+            $this->messageManager->addErrorMessage(__('Please select indexers'));
+            return $resultRedirect;
+        }
+
+        try {
+            $startTime = microtime(true);
+            foreach ($indexerIds as $indexerId) {
+                $indexer = $this->indexerFactory->create()
+                    ->load($indexerId);
+                $indexer->reindexAll();
+            }
+            $endTime = microtime(true) - $startTime;
+            $this->messageManager->addSuccessMessage(__(
+                '%1 indexer(s) have been rebuilt successfully in %2 seconds',
+                count($indexerIds),
+                \gmdate('s', $endTime)
+            ));
+        } catch (\Exception $e) {
+            $this->messageManager->addExceptionMessage(
+                $e,
+                __("Couldn't reindex because of an error.")
+            );
+        }
+
+        return $resultRedirect;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function _isAllowed()
+    {
+        if ($this->_request->getActionName() == 'massReindex') {
+            return $this->_authorization->isAllowed('Magento_Indexer::changeMode');
+        }
+
+        return false;
+    }
+}
